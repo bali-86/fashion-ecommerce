1aefb4024b7fbe786c372ea901c3b33ef8157570
bali-86 <muhammadbilalafzal86@gmail.com>
2023-01-17 02:50:02 -0800

    fix footer issue

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,22 +1,16 @@
 <template>
-  <Header />
   <HeroImg />
   <AllProduct />
-  <TheFooter />
 </template>
 <script>
 // import all the component in home page and the import this homepage in router
-import Header from "../components/layouts/TheHeader.vue";
 import HeroImg from "../components/HeroImg.vue";
 import AllProduct from "../components/AllProduct.vue";
-import TheFooter from "../components/layouts/TheFooter.vue";
 
 export default {
   components: {
-    Header,
     HeroImg,
     AllProduct,
-    TheFooter,
   },
 };
 </script>
